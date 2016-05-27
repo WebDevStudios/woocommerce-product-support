24cec6cabdf781837b543cf9b55f2b53ceb89b4b
Michael Beckwith <tw2113@gmail.com>
2016-05-27 13:15:29 -0500

    unit test code

---
+++ tests/WOO_PROD_SUPPORT-Utilities-Test.php
@@ -0,0 +1,29 @@
+<?php
+require_once( 'WOO_PROD_SUPPORT-Base-Tests.php' );
+
+class WOO_PROD_SUPPORT_template_tags extends WOO_PROD_SUPPORT_Base_Tests {
+
+	/**
+	 * Set up
+	 */
+	public function setUp() {
+		parent::setUp();
+	}
+
+	/*
+	 * Teardown.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * Tests for our inc files being present and available.
+	 */
+	public function test_WOO_PROD_SUPPORT_loader_exist() {
+		$this->assertFileExists( WOO_PROD_SUPPORT_DIRECTORY_PATH . '/bbp-content-restriction.php' );
+		$this->assertFileExists( WOO_PROD_SUPPORT_DIRECTORY_PATH . '/edd-product-support.php' );
+		$this->assertFileExists( WOO_PROD_SUPPORT_DIRECTORY_PATH . '/readme.txt' );
+		$this->assertFileExists( WOO_PROD_SUPPORT_DIRECTORY_PATH . '/upgrade.php' );
+	}
+}
